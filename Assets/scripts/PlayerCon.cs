67899c62c0cae14b393b876a723da66060c4afb6
Sam Martino <sam.martino417@gmail.com>
2018-10-17 10:31:53 -0700

    Upload

---
+++ Assets/scripts/PlayerCon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCon : MonoBehaviour {
+	public float speed;
+	private Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate()
+	{
+		float moveHoizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 Movement = new Vector3 (moveHoizontal, 0.0f, moveVertical);  
+
+
+
+		rb.AddForce (Movement * speed);
+			
+	}
+
+}
